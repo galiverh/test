f529d141cf7457fefcb3509159a87203a674f0f4
galiverh <92427966+galiverh@users.noreply.github.com>
2021-10-13 22:35:18 +0330

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 var http = require('http');
-port = process.env.PORT || process.argv[2];
+port = process.env.PORT || process.argv[2] || 80;
 http.createServer(function (request, response) {
     const proxy = require('request');
-    proxy.get(request.url)
-        .on('response', function(res) {
-            res.on('data', function(data) {
-                    response.write(data)
-            })
-            .on('end',()=>{
-                response.end()
-            })
+    proxy.get(request.url,(e,r,b)=>if(e)response.end(e.toString()))
+        .on('response', (res,err)=>{
+
+            res.on('data', (d)=>response.write(d))
+            .on('end',()=>response.end())
+            .on('error',(e)=>console.log(e))
     })
 }).listen(port,()=>console.log('listTo'+port));
