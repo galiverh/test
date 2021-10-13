1b2d53c57027bc897b66ff94aa855b119061b9d4
galiverh <92427966+galiverh@users.noreply.github.com>
2021-10-13 14:08:41 +0330

    Update server.js

--- server.js
+++ server.js
@@ -1,47 +1,10 @@
 var http = require('http');
-
-port = process.env.PORT || process.argv[2] || 8080;
-http.createServer(function(request, response) {
-  var options = {
-    hostname: 'www.youtube.com',
-    port: 80,
-    path: request.url,
-    method: request.method
-  }
-
-  // do proxy request
-  var proxy = http.request(options, function (res) {
-
-    // response back from proxy request
-    res.setEncoding('utf8');
-    res.on('data', function (chunk) {
-
-      // output to browser
-      response.write(chunk, 'binary');
+port = process.env.PORT || process.argv[2] || 8081;
+http.createServer(function (req, res) {
+    const request = require('request');
+    request('http://www.google.com', function (error, response, body) {
+        res.writeHead(response.statusCode, response.headers);
+        res.write(body)
+        res.end()
     });
-
-    res.on('end', function() {
-      // end output to browser
-      response.end();
-      proxy.end();
-    });
-
-    response.writeHead(res.statusCode, res.headers);
-  });
-
-  // handle error
-  proxy.on('error', function(e) {
-    reponse.write("problem with request: " + e.message);
-    reponse.end();
-  });
-
-  // write data to request body
-  request.on('data', function(chunk) {
-    proxy.write(chunk, 'binary');
-  });
-
-  request.on('end', function() {
-    proxy.end();
-  });
-
 }).listen(port);
