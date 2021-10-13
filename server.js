6063ed64696693a1b306e4f7153cf721b4c0464a
galiverh <92427966+galiverh@users.noreply.github.com>
2021-10-13 12:23:55 +0330

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+// using the http module
+let http = require('http'),
+ 
+// look for PORT environment variable, 
+// else look for CLI argument,
+// else use hard coded value for port 8080
+port = process.env.PORT || process.argv[2] || 8080;
+ 
+// create a simple server
+let server = http.createServer(function (req, res) {
+ 
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+        res.write('hello heroku!', 'utf-8');
+        res.end();
+ 
+    });
+ 
+// listen on the port
+server.listen(port, function () {
+ 
+    console.log('app up on port: ' + port);
+ 
+});
