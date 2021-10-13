316eaef57222dba7dc32487292af630746f2ad59
galiverh <92427966+galiverh@users.noreply.github.com>
2021-10-13 15:05:03 +0330

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 var http = require('http');
 port = process.env.PORT || process.argv[2] || 8081;
-http.createServer(function (req, res) {
-    const request = require('request');
-    request('https://youtube.com', function (error, response, body) {
-        res.writeHead(response.statusCode, response.headers);
-        res.write(body)
-        res.end()
-    });
+http.createServer(function (request, response) {
+
+    const proxy = require('request');
+    // proxy('https://www.youtube.com'+request.url,(err,res,b) =>{
+    //     res.on('data',d=>response.write(d))
+    //     res.on('end',d=>response.end())
+    // });
+    
+    proxy.get('https://www.google.com'+request.url)
+        .on('response', function(res) {
+
+            res.on('data', function(data) {
+                response.write(data)
+            })
+                .on('end',()=>response.end())
+    })
+
+
+
 }).listen(port);
