4a8ce4e1b67bfb784abc93efc2c44ce29966bc8c
galiverh <92427966+galiverh@users.noreply.github.com>
2021-10-13 14:15:06 +0330

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 port = process.env.PORT || process.argv[2] || 8081;
 http.createServer(function (req, res) {
     const request = require('request');
-    request('http://www.google.com', function (error, response, body) {
+    request('https://google.com', function (error, response, body) {
         res.writeHead(response.statusCode, response.headers);
         res.write(body)
         res.end()
